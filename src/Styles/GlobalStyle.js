17ab2f17d35990239310c77824b4b329f139c5ff
mooseon <whomsp@gmail.com>
2022-02-14 17:55:39 +0900

    Refactor: 초기세팅 수정

--- src/Styles/GlobalStyle.js
+++ src/Styles/GlobalStyle.js
@@ -3,9 +3,8 @@ import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
-	
-	// 전역스타일
-	*{
+
+  * {
     box-sizing: border-box;
     font-family:'Spoqa Han Sans Neo', 'sans-serif';
   }
