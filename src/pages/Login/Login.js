8f7eba9c6ef03ee1619b323f4586b7c223b144eb
yejijang <wkd7791@gmail.com>
2022-02-22 21:04:06 +0900

    add:logOut&profile

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,12 +2,26 @@ import React from 'react';
 
 function Login() {
   const [modalOpen, setModalOpen] = useState(false);
+  const [profile, setProfile] = useState();
 
+  //modal
   const modalHandler = () => {
     setModalOpen(prev => !prev);
   };
 
+  //token
+  const logOutHandler = () => {
+    setModalOpen(prev => !prev);
+    if (sessionStorage.getItem('token')) {
+      sessionStorage.removeItem('token');
+      alert('로그아웃 되었습니다.');
+      navigate('/');
+      return modalHandler();
+    }
+  };
+
   const navigate = useNavigate();
+
   const kakaoLoginClickHandler = () => {
     Kakao.Auth.loginForm({
       success: function (authObj) {
@@ -38,9 +52,31 @@ function Login() {
     });
   };
 
+  Kakao.API.request({
+    url: '/v2/user/me',
+    data: {
+      property_keys: ['properties.thumbnail_image'],
+    },
+    success: function (response) {
+      const thumbNail = response.properties.thumbnail_image;
+      console.log(thumbNail);
+      setProfile(thumbNail);
+    },
+    fail: function (error) {
+      console.log(error);
+    },
+  });
+
   return (
     <>
-      <HandleModar onClick={modalHandler}>회원가입/로그인</HandleModar>
+      <HandleModar onClick={logOutHandler}>
+        {!sessionStorage.getItem('token') ? (
+          '로그인'
+        ) : (
+          <Profile src={profile} alt="profile" />
+        )}
+      </HandleModar>
+
       <Modal
         isOpen={modalOpen}
         onRequestClose={modalHandler}
@@ -81,4 +117,154 @@ function Login() {
   );
 }
 
+<<<<<<< HEAD
+=======
+const modalStyle = {
+  overlay: {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'rgba(230, 230, 230, 0.45)',
+    zIndex: 10,
+  },
+  content: {
+    display: 'flex',
+    position: 'absolute',
+    top: 100,
+    left: 500,
+    flexDirection: 'column',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    width: '400px',
+    height: '420px',
+    zIndex: 10,
+  },
+};
+
+const ModalTop = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 16px 20px;
+  width: 385px;
+  height: 54px;
+`;
+
+const Logo = styled.img`
+  display: flex;
+  justify-content: center;
+  margin-left: 145px;
+  width: 70px;
+  height: 16px;
+`;
+
+const ModalOffBt = styled.button`
+  display: flex;
+  justify-content: center;
+  background-color: transparent;
+  border: none;
+  width: 24px;
+  height: 24px;
+  cursor: pointer;
+`;
+
+const ModalOut = styled.img`
+  display: flex;
+  width: 15px;
+  height: 15px;
+  justify-content: end;
+`;
+
+const ModalBody = styled.div`
+  width: 385px;
+  height: 626px;
+  padding: 20px;
+`;
+
+const Title = styled.div`
+  font-size: 26px;
+  font-weight: 600;
+  text-align: center;
+  line-height: 1.54;
+`;
+
+const Descript = styled.div`
+  margin-top: 16px;
+  line-height: 1.5;
+  font-size: 16px;
+  font-weight: 400;
+  text-align: center;
+  color: #666;
+`;
+
+const SocialLog = styled.div`
+  width: 345px;
+  height: 84px;
+  margin-top: 30px;
+  display: flex;
+  justify-content: center;
+`;
+
+const SocialWrap = styled.div`
+  width: 70px;
+  height: 80px;
+  margin: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const SocialBtWrapFirst = styled.button`
+  width: 56px;
+  height: 56px;
+  margin: 0 auto 8px;
+  background-color: yellow;
+  border-radius: 28px;
+  border: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+`;
+
+const SocialBtWrapSecond = styled.button`
+  width: 56px;
+  height: 56px;
+  margin: 0 auto 8px;
+  background-color: white;
+  border-radius: 28px;
+  border: 1px solid lightgray;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+`;
+
+const SocialLogo = styled.img`
+  width: 23px;
+  height: 23px;
+`;
+
+const SocailTitle = styled.div`
+  width: 60px;
+  height: 19px;
+  text-align: center;
+`;
+
+const HandleModar = styled.button`
+  border: none;
+  background-color: transparent;
+  font-size: 14px;
+  cursor: pointer;
+`;
+
+const Profile = styled.img`
+  width: 28px;
+  height: 28px;
+  border-radius: 60%;
+`;
+
+>>>>>>> 4e1bb00 (add:logOut&profile)
 export default Login;
